c7f330a63175abbfbcb9577154aa36f89aeb21e8
rodrigo de Freitas <rodfrei@gmail.com>
2020-02-04 17:08:45 -0300

    More Implatations

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  host: 'rajje.db.elephantsql.com',
+  username: 'tzkwpxkh',
+  password: 'NaYcEBdD0HJ5U7cSgzGsFs-w8lS-bXQJ',
+  database: 'tzkwpxkh',
   define: {
     timestamps: true,
     underscored: true,

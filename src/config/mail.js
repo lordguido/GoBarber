43898346e3bf15cfc542a49a253e7045d871050c
rodrigo Freitas <rodfrei@gmail.com>
2020-02-08 01:31:14 -0300

    NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3aee1c9a9d7d87',
+    pass: '525da61772c617',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
